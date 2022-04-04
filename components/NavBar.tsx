e1066e519fe4ca40afb3918bf64835d0be34fa01
jmossesgeld <jmossesgeld@gmail.com>
2022-04-05 04:23:56 +0800

    update NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,22 @@
-import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+const NavLink = ({ children, ...props }) => (
+  <a className="cursor-pointer hover:text-blue-400 hidden md:inline" {...props}>
+    {children}
+  </a>
+);
 
 export default function NavBar() {
   return (
-    <div className="fixed top-0 h-12 w-full bg-violet-100">
-      <div className="flex space-x-2 justify-end mx-2 items-center h-full">
-        <a>Home</a>
-        <a>Products</a>
-        <a>About</a>
-        <a>Contact</a>
+    <div className="fixed flex justify-around items-center top-0 h-12 w-full bg-slate-800 text-slate-50">
+      <input placeholder=" Search" className="w-1/3 min-w-fit text-slate-800 px-2 py-1" />
+      <div className="flex space-x-8 justify-end items-center h-full">
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/">Products</NavLink>
+        <NavLink href="/cart">
+          <i className="fa-solid fa-cart-shopping"></i> Cart
+        </NavLink>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,9 @@ import NavBar from "../components/NavBar";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Mage Co. Apparel</title>
+      </Head>
       <NavBar />
     </div>
   );
