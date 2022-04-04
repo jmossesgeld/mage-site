38d04b27917dd0782f3a8baa1b8e309793f1f24e
jmossesgeld <jmossesgeld@gmail.com>
2022-04-05 04:21:41 +0800

    update font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: { sans: "Poppins, Arial, sans-serif", serif: "Georgia, serif", mono: "mono" },
     extend: {},
   },
   plugins: [],
