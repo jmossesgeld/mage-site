06b09effdd890561c9a61d3847bcc485da26a842
jmossesgeld <jmossesgeld@gmail.com>
2022-04-13 01:50:41 +0800

    update product page ui

--- lib/data/productList.tsx
+++ lib/data/productList.tsx
@@ -23,6 +23,30 @@ const productList = [
     price: 90,
     imgsrc: "https://cf.shopee.ph/file/31f54086cb2e410705da423e42eb6c85_tn",
   },
+  {
+    id: 5,
+    title: "Premium Demon Slayer Tshirt (Unisex)",
+    price: 230,
+    imgsrc: "https://cf.shopee.ph/file/d740b81f3794d256f4cf350479a49d5e",
+  },
+  {
+    id: 6,
+    title: "Zara plain Polo Top Pastel",
+    price: 120,
+    imgsrc: "https://cf.shopee.ph/file/b6628d02f078ee184e4e69ff347fe547_tn",
+  },
+  {
+    id: 7,
+    title: "Korean puff crop top",
+    price: 90,
+    imgsrc: "https://cf.shopee.ph/file/68d50a7a1f35c1362dac65377c552703_tn",
+  },
+  {
+    id: 8,
+    title: "Collared shirt for women",
+    price: 90,
+    imgsrc: "https://cf.shopee.ph/file/31f54086cb2e410705da423e42eb6c85_tn",
+  },
 ];
 
 export default productList;
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,11 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
+import React from "react";
 import { GetStaticPaths, GetStaticProps } from "next";
+import NavBar from "../../components/NavBar";
 import productList from "../../lib/data/productList";
 
 const Button = ({ children, className, ...props }: any) => (
   <button
     className={
-      "rounded-lg p-2 box-content text-xs text-slate-600 min-w-[25%] bg-rose-500 " + className
+      "rounded-lg p-2 box-content text-xs text-slate-600 min-w-[25%] bg-rose-500 hover:bg-rose-600 hover:text-rose-50 " +
+      className
     }
     {...props}
   >
@@ -14,18 +17,51 @@ const Button = ({ children, className, ...props }: any) => (
 );
 
 export default function ProductPage({ imgsrc, title, price }) {
+  const sizes = ["S", "M", "L", "XL", "XXL"];
+  const [selectedSize, setSelectedSize] = React.useState(sizes[0]);
+
   return (
-    <div>
-      <img className="mx-auto h-full  object-contain" src={imgsrc} alt={title} />
-      <p>{title}</p>
-      <p>{price}</p>
-      <div className="flex justify-between">
-        <Button className="text-rose-600 border-rose-200 border-2 bg-rose-50">
-          <i className="fa-solid fa-cart-shopping"></i> Add to Cart
-        </Button>
-        <Button className="text-rose-50">Buy</Button>
+    <>
+      <NavBar />
+      <div className="flex flex-wrap w-[90%] mt-24 mx-auto max-w-4xl border-2 justify-start  p-4">
+        <img className="object-contain w-full sm:w-1/2" src={imgsrc} alt={title} />
+        <div className="w-full sm:w-1/2">
+          <p>{title}</p>
+          <p className="text-lg mt-2 text-rose-500">₱{price}</p>
+          <div className="mt-6 after:content-['pcs'] after:position:absolute after:text-black after:inline-block after:ml-2">
+            <input
+              className="w-12 border-2 px-2 py-1"
+              type="number"
+              placeholder="Quantity"
+              min={1}
+              max={10}
+              defaultValue={1}
+            />
+          </div>
+          <div className="mt-8">
+            <span>Sizes:</span>
+            {["S", "M", "L", "XL", "XXL"].map((size: string, idx) => (
+              <button
+                key={idx}
+                className={
+                  "ml-2 border-2 w-8 inline-block text-center rounded-lg " +
+                  (selectedSize === size && "bg-rose-300 text-white")
+                }
+                onClick={() => setSelectedSize(size)}
+              >
+                {size}
+              </button>
+            ))}
+          </div>
+          <div className="flex space-x-4 mt-8">
+            <Button className="text-rose-600 border-rose-200 border-2 bg-rose-50 ">
+              <i className="fa-solid fa-cart-shopping"></i> Add to Cart
+            </Button>
+            <Button className="text-rose-50">Buy</Button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -44,7 +80,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   // Get the product data for the specified product id
   const data = productList.find((product) => product.id.toString() === params.id);
-  
+
   return {
     props: data,
   };
